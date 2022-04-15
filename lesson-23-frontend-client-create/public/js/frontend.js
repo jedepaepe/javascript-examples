b189a5375e33ca34b6dcf4c58e46825001ff7740
jedepaepe <jedepaepe@epfc.eu>
2022-04-15 16:55:33 +0200

    implentation of delete

--- lesson-23-frontend-client-create/public/js/frontend.js
+++ lesson-23-frontend-client-create/public/js/frontend.js
@@ -1,13 +1,33 @@
 fetch('api/games')
 .then(response => response.json())
 .then(data => {
-    const content = document.getElementById('content');
-    if (! data || ! data.length || data.length == 0) {
-        content.innerHTML = '<p>Pas de parties'
-    } else {
-        const thead = '<tr>' + Object.keys(data[0]).map(k => '<th>' + k).join('');
-        const tbody = data.map(d => '<tr>' + Object.values(d).map(c => '<td>' + c).join('')).join('');
-        content.innerHTML = `<table class="table table-striped"><thead>${thead}<tbody>${tbody}`;
-    }
+    document.getElementById('content').innerHTML = buildTable(data);
 })
-.catch(error => console.log(error));
+.catch(error => console.error(error));
+
+function buildTable(games) {
+    if (! games || ! games.length || games.length === 0) {
+        return '<p>Pas de parties'
+    }
+    const thead = '<tr>' + Object.keys(games[0]).map(k => '<th>' + k).join('') + '<td>';
+    const tbody = games.map(game => 
+            '<tr>' + 
+            Object.values(game)
+                .map(property => '<td class="align-middle">' + property)
+                .join('') + 
+            '<td><button class="btn btn-danger" onclick="onDeleteHandler(this)">supprimer'
+        ).join('');
+    return `<table class="table table-striped"><thead>${thead}<tbody>${tbody}`;
+}
+
+function onDeleteHandler(button) {
+    const tr = button.parentNode.parentNode;
+    const id = tr.firstChild.innerText;
+    fetch(`api/games/${id}`, { method: 'DELETE' })
+    .then(data => {
+        console.log(`${id} supprimé`);
+        tr.remove();
+    })
+    .catch(error => console.error(error));
+    return false;
+}
