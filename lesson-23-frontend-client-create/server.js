b48ba96728e6aeeceaa5388c6424fae67a7f76e3
jedepaepe <jedepaepe@epfc.eu>
2022-04-21 20:10:05 +0200

    update

--- lesson-23-frontend-client-create/server.js
+++ lesson-23-frontend-client-create/server.js
@@ -47,7 +47,7 @@ client.connect( (err, client) => {
     app.put('/api/games/:id', (req, res) => {
         let id = req.params.id;
         let game = req.body;
-        client.db('tictactoe').collection('games').replaceOne({_id: ObjectId(req.params.id)}, game, (error, result) => {
+        client.db('tictactoe').collection('games').replaceOne({_id: ObjectId(id)}, game, (error, result) => {
             if (error) {
                 console.error(error);
                 res.statusCode = 500;
@@ -55,6 +55,20 @@ client.connect( (err, client) => {
             }
             res.statusCode = 204; // la ressource a été mise à jour avec succès
             res.json({success: true, message: 'document updated'});
+        });
+    });
+
+    // HTTP GET pour Read un document
+    app.get('/api/games/:id', (req, res) => {
+        let id = req.params.id;
+        client.db('tictactoe').collection('games').findOne({_id: ObjectId(id)}, (error, result) => {
+            if (error) {
+                console.error(error);
+                res.statusCode = 500;
+                res.json({success: false, message: 'error'});
+            }
+            res.statusCode = 200;
+            res.json(result);
         });
     });
 
